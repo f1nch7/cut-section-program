8b55d3831e3151752efd48be12a52c658886d912
HaroldFinch <8940520+harold_finch@user.noreply.gitee.com>
2024-12-26 11:37:31 +0800

    筛选断面网格算法已完成

--- cut-section-program/src/SectionData.cpp
+++ cut-section-program/src/SectionData.cpp
@@ -22,7 +22,6 @@ void SectionData::initSectionData(const vector<Point>& _vPoints) {
 	for (size_t i = 0; i < (_vPoints.size() / 2); i++) {
 		double k = (_vPoints[i * 2 + 1].y - _vPoints[i * 2].y) / (_vPoints[i * 2 + 1].x - _vPoints[i * 2].x);
 		double b = _vPoints[i * 2].y - k * _vPoints[i * 2].x;
-		//cout << "k = " << k << " b = " << b << endl;
 		this->calSectionData(k, b);
 	}
 	for (size_t i = 0; i < this->allSections.size(); i++) {
@@ -38,25 +37,23 @@ void SectionData::calSectionData(double _k, double _b) {
 		传入的k和b是断面的直线方程的k和b
 		然后根据bathy文件的头部信息，获取到原点坐标xorign,yorigin
 		将xorign + cellsize * x作为横坐标代入直线方程，得到y值
-		判断得到的y是几倍的cellsize，即为行号，1.1倍的cellsize即为第二行
+		判断得到的y是几倍的cellsize，即为行号，1.1倍的cellsize即为第1行
 		
 	*/
 	// y1 = k * x1 + b
 	vector<Point> vSections;
 	for (size_t i = 0; i < this->gr.ncols; i++) {
-		cout << "i = " << i << endl;
-		double y = _k * (this->gr.xllcorner + this->gr.cellSize * i) + _b;
-		double yushu = (y - this->gr.yllcorner) / this->gr.cellSize;
-		cout << "yushu = " << yushu << endl;
-		int nrow = int (yushu);
-		// 由于坐标表示的是网格的中心点，所以判断的时候，要加上0.5
-		if (y - this->gr.yllcorner - nrow * this->gr.cellSize > this->gr.cellSize / 2) {
-			nrow++;
-		}
+		//cout << "当前X是 i = " << i << endl;
+		//cout << "断面直线 y = " << _k << "x + " << _b << endl;
+		double newY = _k * (this->gr.xllcorner + this->gr.cellSize * i) + _b; // 计算出当前X对应的Y
+		//cout << "网格的x代入直线得到newY = " << newY << endl;
+		int nrow = (newY - this->gr.yllcorner) / this->gr.cellSize; // 计算上几倍的网格精度
 		// 根据行列号，计算出对应的坐标
-		Point p = this->gr.getPointCoor(i, nrow);
-		//cout << "nrow = " << nrow << " i = " << i << endl;
-		vSections.push_back(p);
+		if (this->isSectionData(nrow, i, _k, _b)) {
+			Point p = this->gr.getPointCoor(nrow, i);
+			vSections.push_back(p);
+		}
+		
 		if (this->isSectionData(nrow + 1, i, _k, _b)) {
 			Point p1 = this->gr.getPointCoor(nrow + 1, i);
 			//cout << "nrow + 1 = " << nrow + 1 << " i = " << i << endl;
@@ -67,6 +64,7 @@ void SectionData::calSectionData(double _k, double _b) {
 			//cout << "nrow - 1 = " << nrow - 1 << " i = " << i << endl;
 			vSections.push_back(p2);
 		}
+
 		//system("pause");
 	}
 	this->allSections.push_back(vSections);
@@ -74,27 +72,60 @@ void SectionData::calSectionData(double _k, double _b) {
 
 bool SectionData::isSectionData(double _nrow, double _ncol, double _k, double _b)
 {
-	// 根据传入的行列号,先得到对应的坐标
-	Point p = this->gr.getPointCoor(_nrow, _ncol);
-	cout << "p.x = " << p.x << " p.y = " << p.y << endl;
-	// 坐标如果有小于原点的横坐标或者纵坐标，直接返回false
-	if (p.x < this->gr.xllcorner || p.y < this->gr.yllcorner) {
-		return false;
-	}
+	//// 根据传入的行列号,先得到对应的坐标
+	//Point current_point = this->gr.getPointCoor(_nrow, _ncol);
+	//cout << "相邻网格 p.x = " << current_point.x << " p.y = " << current_point.y << endl;
+	//// 坐标如果有小于原点的横坐标或者纵坐标，直接返回false
+	//if (current_point.x < this->gr.xllcorner || current_point.y < this->gr.yllcorner) {
+	//	cout << "coor error" << endl << endl;
+	//	return false;
+	//}
 
-	// 然后根据直线方程，判断这个点到直线的距离是否小于根号二的cellsize/2，是则返回true，否则返回false
-	// 直线方程是y = kx + b
-	// 到直线的距离是 abs(kx - y + b) / sqrt(k^2 + 1)
-	double dis = abs(_k * p.x - p.y + _b) / sqrt(_k * _k + 1);
-	cout << "_ncol = " << _ncol << " _nrow = " << _nrow << endl;
-	cout << "_k = " << _k << " _b = " << _b << endl;
-	cout << "p.x = " << p.x << " p.y = " << p.y << endl;
-	cout << "d2s = " << sqrt(2) * this->gr.cellSize / 2 << endl;
-	cout << "dis = " << dis << endl;
-	system("pause");
-	//if (dis < 1.414214 * this->gr.cellSize / 2) {
-	if (dis < sqrt(2) * this->gr.cellSize / 2) {		
+	//// 然后根据直线方程，判断这个点到直线的距离是否小于根号二的cellsize/2，是则返回true，否则返回false
+	//// 直线方程是y = kx + b
+	//// 到直线的距离是 abs(kx - y + b) / sqrt(k^2 + 1)
+	//double dis = abs(_k * current_point.x - current_point.y + _b) / sqrt(_k * _k + 1);
+	//cout << "_ncol = " << _ncol << " _nrow = " << _nrow << endl;
+	//cout << "_k = " << _k << " _b = " << _b << endl;
+	//cout << "isS p.x = " << current_point.x << " isS p.y = " << current_point.y << endl;
+	//cout << "d2s = " << sqrt(2) * this->gr.cellSize / 2 << endl;
+	//cout << "dis = " << dis << endl << endl;
+	////if (dis < 1.414214 * this->gr.cellSize / 2) {
+	//if (dis < sqrt(2) * this->gr.cellSize / 2) {
+	//	cout << "zhengque!" << endl << endl;
+	//	return true;
+	//}
+	//cout << "chaochaochaole!" << endl << endl;
+	//return false;
+	double left = this->gr.cellSize * _ncol;
+	double right = this->gr.cellSize * _ncol + this->gr.cellSize;
+	double bottom = this->gr.cellSize * _nrow;
+	double top = this->gr.cellSize * _nrow + this->gr.cellSize;
+
+	// 检查直线是否与网格的左边界相交
+	double yLeft = _k * left + _b;
+	if (yLeft >= bottom && yLeft <= top) {
 		return true;
 	}
+
+	// 检查直线是否与网格的右边界相交
+	double yRight = _k * right + _b;
+	if (yRight >= bottom && yRight <= top) {
+		return true;
+	}
+
+	// 检查直线是否与网格的下边界相交
+	double xBottom = (bottom - _b) / _k;
+	if (xBottom >= left && xBottom <= right) {
+		return true;
+	}
+
+	// 检查直线是否与网格的上边界相交
+	double xTop = (top - _b) / _k;
+	if (xTop >= left && xTop <= right) {
+		return true;
+	}
+
+	// 如果直线与网格的任何一边都没有交点，则不穿过网格
 	return false;
 }
